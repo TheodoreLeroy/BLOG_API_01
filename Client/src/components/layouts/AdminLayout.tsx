f1906dafe54a210659173c556b4d0dd903b0ffd9
TheodoreLeroy <101232135+TheodoreLeroy@users.noreply.github.com>
2026-04-13 17:46:52 +0700

    Delete Client/src directory

--- Client/src/components/layouts/AdminLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function AdminLayout() {
-    const [isOpen, SetIsOpen] = useState<boolean>(true);
-
-    return <div id="app-container" className="w-full h-full"></div>;
-}
-
-export default AdminLayout;
